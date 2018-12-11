66c1b1351d02637a0de0e29b5303176ca051d7fe
ondarcz <ondarcz@seznam.cz>
2018-12-11 22:50:55 +0100

    populate product into order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ const Product =require('../models/product');
 router.get('/', (req, res, next) => {
 	Order.find()
 		.select('product quantity _id')
+		.populate('product','name') // get details of product, select name property
 		.exec()
 		.then(docs => {
 			res.status(200).json({
@@ -73,6 +74,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
 	Order.findById(req.params.orderId)
+		.populate('product','name')
 		.exec()
 		.then(order => {
 			if(!order){
