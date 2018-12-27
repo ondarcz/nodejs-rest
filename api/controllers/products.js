ed1c542a4b89cdf8bf2c3bc89c2e3fd8a90f7e65
ondarcz <ondarcz@seznam.cz>
2018-12-27 11:33:13 +0100

    routes

---
+++ api/controllers/products.js
@@ -0,0 +1,153 @@
+const Product = require('../models/product')
+const mongoose = require('mongoose');
+
+exports.products_get_all = (req, res, next) => {
+	Product.find()
+		.select('name price _id productImage')
+		.exec()
+		.then(docs =>{
+			const response = {
+				count: docs.length,
+				products: docs.map(doc => {
+					return{
+						name: doc.name,
+						price: doc.price,
+						productImage: doc.productImage,
+						_id: doc._id,
+						request:[
+							{ type: 'GET', url: 'http://localhost:3000/products/' + doc._id },
+							{ type: 'POST', url: 'http://localhost:3000/products/', data: { name: 'String', price: 'Number'} },
+							{ type: 'PATCH', url: 'http://localhost:3000/products/' + doc._id, data:[{propName: 'String', value: 'String'}]},
+							{ type: 'DELETE', url: 'http://localhost:3000/products/' + doc._id }
+						]
+
+					}
+				})
+			};
+			if(docs.length > 0){
+				res.status(200).json(response);
+			} else {
+				res.status(204)
+					.send();
+			
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
+}
+
+exports.products_create_product = (req, res, next)=>{
+	console.log(req.file);
+	if(req.fileValidationError){
+		res.status(409).json({message: req.fileValidationError});
+	}else{
+		const product = new Product({
+			_id: new mongoose.Types.ObjectId(),
+			name: req.body.name,
+			price: req.body.price,
+			productImage: req.file.destination + req.file.filename
+		});
+		product
+			.save()
+			.then(result => {
+				console.log(result);
+				res.status(201).json({
+					message: 'Created product succesfully',
+					createdProduct: {
+						name: result.name,
+						price: result.price,
+						productImage: result.productImage,
+						_id: result._id,
+						request:{
+							type: 'GET',
+							url: 'http://localhost:3000/products/' + result._id
+						}
+					}
+				});
+			})
+			.catch(err => {
+				console.log(err);
+				res.status(500).json({
+					error: err
+				});
+			});
+	}
+}
+
+exports.products_get_product = (req, res, next)=>{
+	const id= req.params.productId;
+	Product.findById(id)
+		.select('name price _id productImage')
+		.exec()
+		.then(doc =>{
+			if(doc){
+				res.status(200).json({
+					product: doc,
+					request:{
+						type: 'GET',
+						description: 'GET_ALL_PRODUCTS',
+						url: 'http://localhost:3000/products'
+					}
+				});
+			}else{
+				res.status(404).json({message: 'No valid entry found for provided ID'});
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({error: err});
+		});
+		
+}
+
+exports.products_update_product = (req, res, next)=>{
+	const id = req.params.productId;
+	const updateOps = {};
+	for (const ops of req.body) {
+		updateOps[ops.propName] = ops.value;
+	}
+	Product.update({ _id: id}, {$set: updateOps })
+		.exec()
+		.then(result => {
+			res.status(200).json({
+				message: 'Product updated',
+				request:{
+					type: 'GET',
+					url: 'http://localhost:3000/products/' + id
+				}
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
+}
+
+exports.products_delete_product = (req, res, next) => {
+	const id = req.params.productId;
+	Product.remove({ _id: id })
+		.exec()
+		.then(result => {
+			res.status(200).json({
+				message:'Product deleted',
+				request: {
+					type: 'POST',
+					url:'http://localhost:3000/products',
+					data: { name: 'String', price: 'Number'}
+					
+				}
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
+}
